141937eca1034cd42af6ef4b0763b9506c204fdc
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 05:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name) {
+  kittens.shift(name);
+}
